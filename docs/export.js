dab150b828ee22b53fdf167ce477a3c0e645c527
Magnus Foldager <m_foldager@hotmail.com>
2023-06-12 09:41:36 +0200

    CSV Download

--- docs/export.js
+++ docs/export.js
@@ -22,21 +22,61 @@ const twHideParent = (sel) => {
 const twToggleParent = (sel) => {
 	$(sel).parent().hasClass('hidden') ? twShowParent(sel) : twHideParent(sel)
 }
+const setButtonStatus = (status) => {
+	if (status === 'loading') {
+		$('#beginExport').attr('disabled', true).text('Loading Properties from HubSpot..')
+	} else if (status === 'exporting') {
+		$('#beginExport').attr('disabled', true).text('Downloading CSV..')
+	} else if (status === 'failed') {
+		$('#beginExport').attr('disabled', false).text('Export failed.. try again?')
+	} else $('#beginExport').attr('disabled', false).text('Begin Export')
+}
 
 // Main Functions
 const beginExport = (accessToken, object, archived) => {
+	setButtonStatus('loading')
+
 	const url = `https://better-hubspot-property-export.onrender.com/properties?accessToken=${accessToken}&object=${object}&archived=${archived}`
 	$.ajax({
 		url,
 		type: 'GET',
 		dataType: 'json',
 		success: function (response) {
+			exportToCsv(response, object)
 			console.log(response)
 		},
 		error: function (xhr, status, error) {
+			setButtonStatus('failed')
+			alert('Export failed. You can try again. See console for more information.')
 			console.log('Error:', error)
 		}
 	})
+}
+
+const exportToCsv = (results, object) => {
+	setButtonStatus('exporting')
+
+	let array = typeof results != 'object' ? JSON.parse(results) : results
+	let csv = 'name,label,calculated,description,createdAt,updatedAt,groupName,fieldType,type,displayOrder,externalOptions,formField,hasUniqueValue,hidden,hubspotDefined,modificationMetadata,options'
+
+	array.forEach(function (item, index) {
+		console.log(item, index)
+		let { name, label, calculated, description, createdAt, updatedAt, groupName, fieldType, type, displayOrder, externalOptions, formField, hasUniqueValue, hidden, hubspotDefined, modificationMetadata, options } = item
+		for (var index in array[i]) {
+			csv += `\r\n${name},${label},${calculated},${description},${createdAt},${updatedAt},${groupName},${fieldType},${type},${displayOrder},${externalOptions},${formField},${hasUniqueValue},${hidden},${hubspotDefined},${modificationMetadata},${options}`
+		}
+	})
+
+	const filename = `${object}-${(new Date().toJSON().slice(0,16)).replace(':','-').replace('T','-')}.csv`
+
+	const csvHref = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv)
+	$('#download').attr({
+		download: filename,
+		href: csvHref,
+		target: '_blank'
+	})
+	$('#download')[0].click()
+	setButtonStatus('')
 }
 
 // On Document Ready
